b1186dbc1b39a5c7424f2bd048b2258dace947ff
Daniel Vanzin <daniel.vanzin@ahgora.com.br>
2022-07-16 21:14:35 -0300

    refactor

--- scripts/Google_Cleaner.user.js
+++ scripts/Google_Cleaner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Google Cleaner
 // @description Moves the top bar (All, Videos, News...) to sidebar, hides "rich search content", old style links
-// @version     3.7
+// @version     3.7.1
 // @author      icetbr
 
 // @include       https://www.google.*/search*
@@ -13,117 +13,86 @@
 // ==/UserScript==
 /* jshint esversion: 6 */
 
-var head = document.getElementsByTagName('head')[0];
+const head = document.querySelectorAll('head')[0];
+const { assign } = Object;
 
-function init() {
-    toggleNavBar();
-    toggleFiltersBar();
-    addLinks();
-    cleanGoogle();
-    jsCleanGoogle();
-}
+const style = styles => assign(document.createElement('style'), { type: 'text/css', textContent: styles });
+const div = attrs => assign(document.createElement('div'), attrs);
 
-var isBarVisible = false;
-var hideNavBarStyle = style("#top_nav, #appbar {display:none;}");
-function toggleNavBar() {
+const createLink = (attrs, onclick, parent) => {
+    if (!parent) return null;
+
+    const link = div(attrs);
+    link.addEventListener('click', onclick, false);
+    parent.append(link);
+    return link;
+};
+
+const doLink = tbsParameter => {
+    const parameters = new window.URLSearchParams(window.location.search);
+    parameters.set('tbs', tbsParameter);
+    document.location.href = `?${parameters.toString()}`;
+};
+
+let isBarVisible = false;
+const hideNavBarStyle = style('#top_nav, #appbar {display:none;}');
+const toggleNavBar = () => {
     if (isBarVisible) {
-        head.removeChild(hideNavBarStyle);
+        hideNavBarStyle.remove();
     } else {
-        head.appendChild(hideNavBarStyle);
+        head.append(hideNavBarStyle);
     }
 
     isBarVisible = !isBarVisible;
-}
-function ensureNavBar() {
-    if (!isBarVisible) return;
-    head.removeChild(hideNavBarStyle);
-    isBarVisible = !isBarVisible;
-}
+};
 
-var isFiltersBarVisible = false;
-var hideFiltersBarStyle = style("#filtersBar {display:none;}");
-function toggleFiltersBar() {
+let isFiltersBarVisible = false;
+const hideFiltersBarStyle = style('#filtersBar {display:none;}');
+const toggleFiltersBar = () => {
     if (isFiltersBarVisible) {
-        head.removeChild(hideFiltersBarStyle);
+        hideFiltersBarStyle.remove();
     } else {
-        head.appendChild(hideFiltersBarStyle);
+        head.append(hideFiltersBarStyle);
     }
 
     isFiltersBarVisible = !isFiltersBarVisible;
-}
+};
 
-function addLinks() {
-    var parent = document.getElementById('rcnt');
+const toggleEnglishOnly = () => {
+    const parameters = new window.URLSearchParams(window.location.search);
+    parameters.has('gl') ? parameters.delete('gl') : parameters.set('gl', 'us');
+    document.location.href = `?${parameters.toString()}`;
+};
 
-    createLink({ id: 'bartoggle', style: 'font-size: 11px; top: 95px; left: 23px; position: absolute; z-index: 999', textContent: 'Toggle topbar' }, toggleNavBar, parent);
+const showPastYearPosts         = () => doLink('qdr:y');
+const showAnyTimePosts          = () => doLink('qdr:');
+const showPastHourPosts         = () => doLink('qdr:h');
+const showPast24HoursPosts      = () => doLink('qdr:d');
+const showPastWeekPosts         = () => doLink('qdr:w');
+const showPastMonthPosts        = () => doLink('qdr:m');
 
-    createLink({ style: 'font-size: 11px; top: 119px; left: 12px; position: absolute; z-index: 999', textContent: 'Toggle english only' }, toggleEnglishOnly, parent);
-    createLink({ style: 'font-size: 11px; top: 140px; left: 33px; position: absolute; z-index: 999', textContent: 'Past year' }, showPastYearPosts, parent);
-    createLink({ style: 'font-size: 11px; top: 161px; left: 53px; position: absolute; z-index: 999', textContent: '+' }, toggleFiltersBar, parent);
+const ensureNavBar = () => {
+    if (!isBarVisible) return;
 
-    var filtersBar = createLink({ id: 'filtersBar', style: 'font-size: 11px; top: 178px; left: 34px; position: absolute; line-height: 18px; z-index: 999' }, null, parent);
+    hideNavBarStyle.remove();
+    isBarVisible = !isBarVisible;
+};
 
-    createLink({ textContent: 'Past year' }, showPastYearPosts, filtersBar);
-    createLink({ textContent: 'Any time' }, showAnyTimePosts, filtersBar);
-    createLink({ textContent: 'Past hour' }, showPastHourPosts, filtersBar);
-    createLink({ textContent: 'Past 24 hours' }, showPast24HoursPosts, filtersBar);
-    createLink({ textContent: 'Past week' }, showPastWeekPosts, filtersBar);
-    createLink({ textContent: 'Past month' }, showPastMonthPosts, filtersBar);
-    createLink({ textContent: 'Custom range' }, showCustomRangePosts, filtersBar);
-}
-
-function toggleEnglishOnly() {
-    const params = new window.URLSearchParams(window.location.search);
-    params.has('gl') ? params.delete('gl') : params.set('gl', 'us');
-    document.location.href = `?${params.toString()}`;
-}
-
-function showPastYearPosts() { doLink("qdr:y"); }
-function showAnyTimePosts() { doLink("qdr:"); }
-function showPastHourPosts() { doLink("qdr:h"); }
-function showPast24HoursPosts() { doLink("qdr:d"); }
-function showPastWeekPosts() { doLink("qdr:w"); }
-function showPastMonthPosts() { doLink("qdr:m"); }
-
-// this keeps breaking, maybe use parent with jsowner=ow86?
-function showCustomRangePosts() {
+// This keeps breaking, maybe use parent with jsowner=ow86?
+const showCustomRangePosts = () => {
     ensureNavBar();
     document.querySelectorAll('[jsaction="EEGHee"]')[0].click();
-    //document.getElementsByClassName('n5Ug4b')[0].style.display = 'block';
+    // Document.getElementsByClassName('n5Ug4b')[0].style.display = 'block';
+};
 
-}
-
-function doLink(tbsParam) {
-    const params = new window.URLSearchParams(window.location.search);
-    params.set('tbs', tbsParam);
-    document.location.href = `?${params.toString()}`;
-}
-
-function createLink(attrs, onclick, parent) {
-    if (!parent) return null;
-    var link = div(attrs);
-    link.addEventListener("click", onclick, false);
-    parent.appendChild(link);
-    return link;
-}
-
-function div(attrs) {
-    return Object.assign(document.createElement('div'), attrs)
-}
-
-function style(styles) {
-    return Object.assign(document.createElement('style'), { type: 'text/css' }, { textContent: styles });
-}
-
-function cleanGoogle() {
-    //.XqFnDf class for bar with Overview, Songs, Albuns, etc
-    document.body.appendChild(style(`
-        /*.ULSxyf,                  Videos and People also Ask sessions */
-        .csDOgf, .eFM0qc {        /* tree dots for more info */
+const cleanGoogle = () => {
+    document.body.append(style(`
+        /*.ULSxyf,                          Videos and People also Ask sessions */
+        .csDOgf, .eFM0qc {                  /* tree dots for more info */
            display: none;
         }
 
-        .g {                      /* spacing between search results */
+        .g {                                /* spacing between search results */
           margin-top: -15px;
         }
 
@@ -133,11 +102,11 @@ function cleanGoogle() {
 
         /* LINKS/TITLE */
 
-        .FxLDp {                  /* sometimes the first entry has an undesired pad */
+        .FxLDp {                             /* sometimes the first entry has an undesired pad */
           padding-left: 0px;
         }
 
-        h3.LC20lb.MBeuO.DKV0Md {  /* force (some) links from side to bellow title */
+        h3.LC20lb.MBeuO.DKV0Md {            /* force (some) links from side to bellow title */
           display: block;
         }
 
@@ -155,29 +124,62 @@ function cleanGoogle() {
           position: inherit;
         }
     `));
-}
+};
 
-function jsCleanGoogle() {
-    const bres = document.getElementById('bres');
-    if (bres.innerText.indexOf('Related searches') > -1) bres.style.display = 'none';
+const needsHiding = text =>
+    text.startsWith('Images') ||
+    text.startsWith('Videos') ||
+    text.startsWith('People also ask') ||
+    text.startsWith('Twitter results') ||
+    text.startsWith('Top stories') ||
+    text.startsWith('Related searches') ||
+    text.trim().split('\n')[0].endsWith(' - YouTube');
 
-    let blocks = document.getElementsByClassName('cLjAic TzHB6b');
-    if (!blocks.length) blocks = document.getElementsByClassName('ULSxyf');
+let hiddenSections;
+const hideUndesiredSections = section => {
+    if (!needsHiding(section.innerText)) return;
 
-    for (const block of blocks) {
-        if (block.innerText.indexOf('Images') === 0) block.style.display = 'none'; // More images?
-        else if (block.innerText.indexOf('Videos') === 0) block.style.display = 'none';
-        else if (block.innerText.indexOf('People also ask') === 0) block.style.display = 'none';
-        else if (block.innerText.indexOf('Twitter Results') === 0) block.style.display = 'none';
-        else if (block.innerText.indexOf('Top stories') === 0) block.style.display = 'none';
-        else if (block.innerText.indexOf('Related searches') === 0) block.style.display = 'none';
-        else {
-            //if (block.innerText !== '<empty string>') cleaned = true
-            // console.log(block.innerText)
-            // console.log('****************************')
-        }
+    hiddenSections.push(section);
+    section.style.display = 'none';
+};
+
+const jsCleanGoogle = () => {
+    hiddenSections = [];
+    const bres = document.querySelector('#bres');
+    if (bres.innerText.includes('Related searches')) {
+        bres.style.display = 'none';
     }
-}
+
+    document.querySelectorAll('.cLjAic.TzHB6b').forEach(hideUndesiredSections);
+    document.querySelectorAll('.ULSxyf').forEach(hideUndesiredSections);
+};
+
+const addLinks = () => {
+    const parent = document.querySelector('#rcnt');
+
+    createLink({ id: 'bartoggle', style: 'font-size: 11px; top: 95px; left: 23px; position: absolute; z-index: 999', textContent: 'Toggle topbar' }, toggleNavBar, parent);
+
+    createLink({ style: 'font-size: 11px; top: 119px; left: 12px; position: absolute; z-index: 999', textContent: 'Toggle english only' }, toggleEnglishOnly, parent);
+    createLink({ style: 'font-size: 11px; top: 140px; left: 33px; position: absolute; z-index: 999', textContent: 'Past year' }, showPastYearPosts, parent);
+    createLink({ style: 'font-size: 11px; top: 161px; left: 53px; position: absolute; z-index: 999', textContent: '+' }, toggleFiltersBar, parent);
+
+    const filtersBar = createLink({ id: 'filtersBar', style: 'font-size: 11px; top: 178px; left: 34px; position: absolute; line-height: 18px; z-index: 999' }, null, parent);
+
+    createLink({ textContent: 'Past year' }, showPastYearPosts, filtersBar);
+    createLink({ textContent: 'Any time' }, showAnyTimePosts, filtersBar);
+    createLink({ textContent: 'Past hour' }, showPastHourPosts, filtersBar);
+    createLink({ textContent: 'Past 24 hours' }, showPast24HoursPosts, filtersBar);
+    createLink({ textContent: 'Past week' }, showPastWeekPosts, filtersBar);
+    createLink({ textContent: 'Past month' }, showPastMonthPosts, filtersBar);
+    createLink({ textContent: 'Custom range' }, showCustomRangePosts, filtersBar);
+};
+
+const init = () => {
+    toggleNavBar();
+    toggleFiltersBar();
+    cleanGoogle();
+    jsCleanGoogle();
+    addLinks();
+};
 
 init();
-
