ff4bfd006500a2460924f914cad822374c505e3b
icetbr <icetbr@yahoo.com.br>
2022-10-02 17:01:55 -0300

    comments: less bold, no gray background, decluttered, compact

--- scripts/Clearer_EffectiveAltruism.org_Forum.user.js
+++ scripts/Clearer_EffectiveAltruism.org_Forum.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Clearer EffectiveAltruism.org Forum
 // @description More familiar, clean and compact forum UX, with an improved topics organization
-// @version     0.8.0
+// @version     0.8.2
 // @author      icetbr
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=effectivealtruism.org
 // @license     MIT
@@ -18,16 +18,66 @@ var style$1 = /*css*/`
 .Layout-main                      { margin-top: -140px; z-index: 2000; }
 .Header-appBar, .PostsList2-posts { box-shadow: unset; }
 .PostsItem2-bottomBorder          { border-bottom: unset; line-height: 1.4rem; }
-.PostsTitle-root                  { font-size: 1.0rem; line-height: 1.0rem; word-break: break-all; }
+.PostsTitle-root                  { font-size: 1.0rem; line-height: 1.0rem; word-break: break-all; display: contents; }
 .PostsItem2-postsItem             { padding-top: 0; padding-bottom: 2px; }
 .PostsItem2-author                { margin-right: 0; max-width: 100px; }
 .PostsItem2MetaInfo-metaInfo      { font-size: 0.8rem; }
 .PostsItem2-title                 { overflow: visible; padding-bottom: 6px; }
 .PostsItemIcons-iconSet           { position: absolute; left: -60px; }
-.LWPopper-default                 { display: none; }
+.LWPopper-default.LWPopper-noMouseEvents                 { display: none; }
+/* .Header-headroom .headroom        { z-index: unset; } */
 
 .PostsItem2-postsItem { padding-bottom: 0px; margin-bottom: -2px }
-.PostsTitle-root { font-size: 12px; color: rgb(34, 34, 34); font-family: verdana, arial, helvetica, sans-serif; }
+.PostsTitle-root { font-size: 12px; color: rgb(34, 34, 34); font-family: verdana, arial, helvetica, sans-serif; display: contents;}
+
+/* Individual Posts */
+.ToCColumn-header {
+  z-index: 1301;
+}
+
+.PostsPagePostFooter-footerSection {
+  display: none;
+}
+.PingbacksList-list, .PingbacksList-loadMore {
+  display:none;
+}
+
+.PingbacksList-title:hover ~ .PingbacksList-list,
+.PingbacksList-title:hover ~ .PingbacksList-loadMore {
+   display:block;
+}
+
+.CommentsNewForm-root { display: none }
+
+.CommentsListSection-newCommentLabel:hover ~ .CommentsNewForm-root {
+   display:block;
+}
+
+.CommentsNewForm-root:hover, .CommentsNewForm-root:focus-within {
+  display: block;
+}
+
+PostsPagePostFooter-footerTagList {
+  margin-bottom: 0;
+}
+
+.CommentsListSection-newComment, .CommentFrame-node {
+  border: unset;
+}
+/* .CommentsItem-body .CommentsListSection-root */
+.ContentStyles-commentBody * {
+  font-family: verdana, arial, helvetica, sans-serif !important;
+  line-height: 20px;
+/*   font-size: 14px; */
+  width: 600px;
+  hyphens: auto;
+/*   font-weight: 400px; */
+/*   line-height: 17.15px; */
+}
+
+.comments-node-even {
+  background-color: white;
+}
 
 `;
 
